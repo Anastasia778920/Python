7ef14a8b945a8ef19324eb68e2823efa1682172b
Настя <zakharovanas0905@gmail.com>
2022-11-27 15:27:56 +0300

    Задача 4

--- дз2/Задача 4.py
+++ дз2/Задача 4.py
@@ -1,3 +1,21 @@
 ## Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
 ## Найдите произведение элементов на указанных позициях. Позиции вводятся с клавиатуры.
 
+from random import Random, randint
+
+N = int(input('Введите число: '))
+numbers = []
+for i in range(N):
+    numbers.append(randint(-N,N+1))
+print(numbers)
+
+def smes(numbers):
+    list = numbers[:]
+    numbers_length = len(list)
+    for i in range(numbers_length):
+        index = randint(0, numbers_length - 1)
+        temp = list[i]
+        list[i] = list[index]
+        list[index] = temp
+    return list
+print(smes(numbers))
