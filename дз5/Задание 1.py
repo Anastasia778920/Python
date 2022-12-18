a0ff3efa20cd6ada7e338e50bb8d1f5470dc50f4
Настя <zakharovanas0905@gmail.com>
2022-12-18 17:50:42 +0300

    Задание 1

--- дз5/Задание 1.py
+++ дз5/Задание 1.py
@@ -1,14 +1,9 @@
 ## Напишите программу, удаляющую из файла все слова, содержащие "абв".
 
-path = ''
-with open(path, 'r', encoding='utf8')as file:
-    text = file.readline().split()
+txt = input("Введите текст через пробел:\n")
+print(f"Исходный текст: {txt}")
+find_txt = "абв"
+lst = [i for i in txt.split() if find_txt not in i]
 
-print(text)
-find = 'абв'
+print(f'Результат: {" ".join(lst)}')
 
-for word in text:
-    if find in word:
-        text.remove((word))
-    
-print(text)
