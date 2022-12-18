786db2f09b065b7fe9b892410be2abd10bcf64b7
Настя <zakharovanas0905@gmail.com>
2022-12-18 14:15:15 +0300

    Задание 2

--- дз4/Задание 2.py
+++ дз4/Задание 2.py
@@ -2,25 +2,19 @@
 ## Пример
 ## - при N=236     ->        [2, 2, 59]
 
-n = int(input('Вваедите число N: '))
-list = []
-a = n 
-if n > a:
-    restart = True
-    while restart:
-        restart = False
-        for i in range (2, n + 1):
-            if n%i == 0:
-                list.append(i)
-                n = int(n/i)
-                restart = True 
-                break 
+num = int(input('Введите натуральное число N: '))
+i = 2 
+lst = []
+old = num 
+while i <= num:
+    if num % i == 0:
+        lst.append(i)
+        num //= i
+        i = 2
+    else:
+        i += 1
 
-    print (f'Простые множители числа {a} - {list}')
-elif n == 1:
-    print(f'Простые множители числа {a} - [1]')
-else:
-    print(f'Не правильное число')
+print(f"множители числа {old} приведены в списке: {lst}")
 
 
 
